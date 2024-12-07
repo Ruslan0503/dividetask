b002bbcd5fcee158697f9b0737aa5e85043c79df
Ruslan0503 <110323730+Ruslan0503@users.noreply.github.com>
2024-12-07 13:38:32 +0500

    Create RoleAndPermissionSeeder.php

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(['name' => 'create task']);
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo(['create task']);
+    }
+}
