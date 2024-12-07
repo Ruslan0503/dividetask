523827eeaa19222b3fe5c32dc47fa1c70712fb31
Ruslan0503 <110323730+Ruslan0503@users.noreply.github.com>
2024-12-07 13:22:18 +0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,6 +43,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    use Notifiable, HasRoles;
 
 }
 
